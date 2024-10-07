183d8c36f39116170890d03d6aaff203e86f2689
ArcticHare105 <66931185+ArcticHare105@users.noreply.github.com>
2024-10-08 00:51:58 +0800

    update training

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,4 @@
 accelerate launch --num_processes=4 --gpu_ids="0,1,2,3" --main_process_port 29300 src/train_s3diff.py \
-    --sd_path="path_to_checkpoints/sd-turbo" \
     --de_net_path="assets/mm-realsr/de_net.pth" \
     --output_dir="./output" \
     --resolution=512 \
