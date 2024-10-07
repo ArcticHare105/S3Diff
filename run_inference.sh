e40f57f82f3e85a55c0e2de993e4a3de2343ec0b
ArcticHare105 <66931185+ArcticHare105@users.noreply.github.com>
2024-10-07 12:10:43 +0800

    Update run_inference.sh

--- run_inference.sh
+++ run_inference.sh
@@ -3,4 +3,5 @@ accelerate launch --num_processes=1 --gpu_ids="0," --main_process_port 29300 src
     --de_net_path="assets/mm-realsr/de_net.pth" \
     --pretrained_path="path_to_checkpoints_folder/model_30001.pkl" \
     --output_dir="./output" \
-    --ref_path="path_to_ground_truth_folder"
+    --ref_path="path_to_ground_truth_folder" \
+    --align_method="wavelet"
