10e2e34133b06891d723c913b09ffd04806447d9
ArcticHare105 <66931185+ArcticHare105@users.noreply.github.com>
2024-10-08 01:01:52 +0800

    update run_inference

--- run_inference.sh
+++ run_inference.sh
@@ -1,7 +1,5 @@
 accelerate launch --num_processes=1 --gpu_ids="0," --main_process_port 29300 src/inference_s3diff.py \
-    --sd_path="path_to_checkpoints/sd-turbo" \
     --de_net_path="assets/mm-realsr/de_net.pth" \
-    --pretrained_path="path_to_checkpoints_folder/model_30001.pkl" \
     --output_dir="./output" \
     --ref_path="path_to_ground_truth_folder" \
     --align_method="wavelet"
